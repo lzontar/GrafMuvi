7b253a2d549c8b0784b6ae0789a3e5bf2ab2a596
lzontar <44132336+lzontar@users.noreply.github.com>
2019-10-02 17:46:10 +0200

    Switched testing framework from MochaJS to Jest, closes #22

---
+++ __test__/first.test.js
@@ -0,0 +1,3 @@
+test('adds 1 + 1 to equal 2', () => {
+  expect(2).toBe(2);
+});
--- test/firstTest.js
+++
@@ -1,9 +0,0 @@
-// Require the built in 'assertion' library
-var assert = require('assert');
-describe('Simple test', function() {
-  // A string explanation of what we're testing
-  it('should return -1 when the value is not present', function(){
-      // Our actual test: -1 should equal indexOf(...)
-    assert.equal(-1, [1,2,3].indexOf(4));
-  });
-});
