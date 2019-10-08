c1d767b98a406d71293d82091c5485e7f9338a34
lzontar <44132336+lzontar@users.noreply.github.com>
2019-10-08 23:42:11 +0200

    Added basic functionality of Node class, closes #43

---
+++ src/structure/node.js
@@ -0,0 +1,19 @@
+import {Edge} from './edge';
+
+// Author: @lzontar
+
+export class Node {
+  constructor(imdbId, title, released) {
+    this.imdbId = imdbId;
+    this.title = title;
+    this.released = released;
+    this.edges = [];
+  }
+
+  connect(node, promotions) {
+    const edge = new Edge(this, node, promotions);
+    this.edges.push(edge);
+    return edge;
+  }
+
+}
