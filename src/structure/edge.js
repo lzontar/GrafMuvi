7c8a13c5656a20e43f8cd3dd853bf7c36306df64
lzontar <44132336+lzontar@users.noreply.github.com>
2019-10-08 23:42:35 +0200

    Added basic functionality of Edge class, closes #44

---
+++ src/structure/edge.js
@@ -0,0 +1,12 @@
+// Author: @lzontar
+
+
+export class Edge {
+
+  constructor(node1, node2, promotions) {
+    this.promotions = promotions;
+    this.node1 = node1;
+    this.node2 = node2;
+  }
+
+}
