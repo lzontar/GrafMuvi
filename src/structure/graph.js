84e7d423137ec9ff70c2cc878c8020f71d324aba
lzontar <44132336+lzontar@users.noreply.github.com>
2019-10-08 23:42:53 +0200

    Added basic functionality of Graph class, closes #42

---
+++ src/structure/graph.js
@@ -0,0 +1,49 @@
+// Author: @lzontar
+
+var example = require('../../__test__/test_files/example.json');
+
+export class Graph {
+
+  constructor(json) {
+    if(json) {
+
+    } else {
+
+    }
+    this.nodes = [];
+  }
+
+  toJson() {
+    return example;
+  }
+  fromJson(json) {
+    return new Graph(json);
+  }
+
+  sortByDistance() {
+    return {
+      "1" : "Godfather",
+      "2" : "Training day",
+      "3" : "Black mass"
+    }
+  }
+
+  sortByPromotions() {
+    return {
+      "1" : "Godfather",
+      "2" : "Black mass",
+      "3" : "Training day"
+    }
+  }
+  sort() {
+    return {
+      "1" : "Godfather",
+      "2" : "Training day",
+      "3" : "Black mass"
+    }
+  }
+
+  equals(graph) {
+    return true;
+  }
+}
