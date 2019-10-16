07148f7ece3ff4627369dcf8b4a2cae99ac711fb
lzontar <44132336+lzontar@users.noreply.github.com>
2019-10-16 22:07:39 +0200

    Added logging configuration file, closes #53

---
+++ src/logging/logger.js
@@ -0,0 +1,44 @@
+const winston = require('winston');
+var appRoot = require('app-root-path');
+
+const logConfiguration = {
+  transports : [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.colorize(),
+        winston.format.simple()
+      )
+    }),
+    new winston.transports.File({
+      filename: `${appRoot}/logs/server.log` ,
+      level: 'error',
+      colorize: true,
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.colorize(),
+        winston.format.simple()
+      )
+    }),
+    new winston.transports.File({
+      filename: `${appRoot}/logs/server.log` ,
+      level: 'info', 
+      colorize: true,
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.colorize(),
+        winston.format.simple()
+      )
+    }),
+  ]
+}
+const logger = winston.createLogger(logConfiguration);
+
+winston.addColors({
+       error: 'red',
+       warn: 'yellow',
+       info: 'cyan',
+       debug: 'green'
+   });
+
+module.exports = logger;
