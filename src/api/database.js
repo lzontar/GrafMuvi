6f37c2d78a94b3bf1ba2f9fff95e1dcf237b7737
lzontar <44132336+lzontar@users.noreply.github.com>
2019-10-17 01:32:23 +0200

    Added functionality for updating movie promotion by id, closes #59

--- src/api/database.js
+++ src/api/database.js
@@ -28,8 +28,8 @@ exports.matchMovieRecommendationsById = function (session, id, callback) {
 
 exports.matchMovieRecommendationsByTitle = function (session, title, released, callback) {
   params = { title: title, released: released };
-  console.log(params['released']);
-  const cypher = 'MATCH path = (x:Movie {title: {title}, released: toInt({released})})-[:SIMILAR*]->(y:Movie) WHERE size(nodes(path)) = size(apoc.coll.toSet(nodes(path))) UNWIND relationships(path) AS  similar RETURN {path: path, length: length(path), sum_promotions: sum(similar.promotions)} as n ORDER BY n.length, n.sum_promotions'
+
+  const cypher = 'MATCH path = (x:Movie {title: released: toInt({released})})-[:SIMILAR*]->(y:Movie) WHERE size(nodes(path)) = size(apoc.coll.toSet(nodes(path))) UNWIND relationships(path) AS  similar RETURN {path: path, length: length(path), sum_promotions: sum(similar.promotions)} as n ORDER BY n.length, n.sum_promotions'
   session.run(cypher, params)
   .then(result => {
       // On result, get count from first record
@@ -46,13 +46,25 @@ exports.matchMovieRecommendationsByTitle = function (session, title, released, c
 }
 
 
-exports.postPromotionById = function (session, id1, id2, downgrade) {
-  return session && id1 && id2 && downgrade !== undefined ? {
-    id1: 'tt0765429',
-    id2: 'tt0353496',
-    promotions: 101
-  } : undefined
+exports.postPromotionById = function (session, request, callback) {
+ params = { id1: request.body.id1, id2: request.body.id2, downgrade: request.body.downgrade !== undefined && request.body.downgrade ? -1 : 1 }
+
+  const cypher = 'MATCH (m1:Movie {imdbId: {id1}})-[s:SIMILAR]-(m2:Movie{imdbId:{id2}}) WITH m1, s, m2 SET s.promotions = s.promotions + {downgrade} RETURN m1, s, m2'
+  session.run(cypher, params)
+  .then(result => {
+      // On result, get count from first record
+      const count = result.records.length;
+      // Log response
+       logger.info(result.records);
+      callback(result.records, 200);
+  })
+  .catch(e => {
+      // Output the error
+      logger.error(e);
+      callback({error: e}, 404);
+  })
 }
+
 exports.postPromotionByTitle = function (session, title1, released1, title2, released2, downgrade) {
   return session && title1 && released1 && title2 && released2 && downgrade !== undefined ? {
     id1: 'tt0765429',
