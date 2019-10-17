7f8b00846181c57122e6ae322be3bc3e8c1f62f5
lzontar <44132336+lzontar@users.noreply.github.com>
2019-10-17 13:39:21 +0200

    Fixes #64

--- src/logging/logger.js
+++ src/logging/logger.js
@@ -1,8 +1,8 @@
-const winston = require('winston');
-var appRoot = require('app-root-path');
+const winston = require('winston')
+var appRoot = require('app-root-path')
 
 const logConfiguration = {
-  transports : [
+  transports: [
     new winston.transports.Console({
       format: winston.format.combine(
         winston.format.timestamp(),
@@ -11,7 +11,7 @@ const logConfiguration = {
       )
     }),
     new winston.transports.File({
-      filename: `${appRoot}/logs/server.log` ,
+      filename: `${appRoot}/logs/server.log`,
       level: 'error',
       colorize: true,
       format: winston.format.combine(
@@ -21,24 +21,24 @@ const logConfiguration = {
       )
     }),
     new winston.transports.File({
-      filename: `${appRoot}/logs/server.log` ,
-      level: 'info', 
+      filename: `${appRoot}/logs/server.log`,
+      level: 'info',
       colorize: true,
       format: winston.format.combine(
         winston.format.timestamp(),
         winston.format.colorize(),
         winston.format.simple()
       )
-    }),
+    })
   ]
 }
-const logger = winston.createLogger(logConfiguration);
+const logger = winston.createLogger(logConfiguration)
 
 winston.addColors({
-       error: 'red',
-       warn: 'yellow',
-       info: 'cyan',
-       debug: 'green'
-   });
+  error: 'red',
+  warn: 'yellow',
+  info: 'cyan',
+  debug: 'green'
+})
 
-module.exports = logger;
+module.exports = logger
