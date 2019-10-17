0a4fb594664ccf68f05573c2951abcb9b49f1313
lzontar <44132336+lzontar@users.noreply.github.com>
2019-10-18 00:33:15 +0200

    Added tests for graph.js, closes #75

--- __test__/graph.test.js
+++ __test__/graph.test.js
@@ -1,86 +1,26 @@
-import { Graph } from '../src/structure/graph'
-
 // Author: @lzontar
-
-// test files
-var example = require('./test_files/example.json')
+const graph = require('../src/structure/graph')
+const examplePromotions = require('./test_files/examplePromotions')
+const exampleRecommendationList = require('./test_files/exampleRecommendationList')
 
 // -------------------------------------UNIT TESTS -------------------------------------//
-describe('Constructor', () => {
-  test('The service should be able to call new() on Graph', () => {
-    const graph = new Graph()
-    // Ensure constructor created the object:
-    expect(graph).toBeTruthy()
+describe('Relationship promotion/downgrade', () => {
+  test('Returns json, that conatains updated relationship, from data returned from database', () => {
+    const sortedJson = {
+      id1: 'tt0120338',
+      id2: 'tt0332280',
+      promotions: 2
+    }
+    expect(graph.toRelationship(examplePromotions)).toStrictEqual(sortedJson)
   })
 })
-describe('Graph comparison', () => {
-  test('Test of Graph.equals() function - empty graphs', () => {
-    const graph1 = new Graph()
-    const graph2 = new Graph()
-
-    expect(graph1.equals(graph2)).toBe(true)
-  })
-
-  test('Test of Graph.equals() function - non-empty graphs', () => {
-    const json = example
-    const graph1 = new Graph(json)
-    const graph2 = new Graph(json)
-
-    expect(graph1.equals(graph2)).toBe(true)
-  })
-})
-
-describe('JSON', () => {
-  test('Test of Graph.fromJson() function', () => {
-    const json = example
-    const graph1 = new Graph(json)
-    const graph2 = new Graph()
-    graph2.fromJson(json)
-
-    expect(graph1.equals(graph2)).toBe(true)
-  })
-
-  test('Test of Graph.toJson() function', () => {
-    const expectedJson = example
-    const graph = new Graph(example)
-    expect(graph.toJson()).toBe(expectedJson)
-  })
-})
-// -------------------------------------INTEGRATION TESTS -------------------------------------//
-describe('Sorting', () => {
-  test('Test function sortByDistance(), graph sort by distance', () => {
-    const unsortedJson = example
-    const sortedJson = {
-      1: 'Godfather',
-      2: 'Training day',
-      3: 'Black mass'
+describe('Movie recommendation list', () => {
+  test('Returns json, that contains a movie recommenadtion list, from data returned from database', () => {
+    const recommendationList = {
+      1: 'test2',
+      2: 'test3',
+      3: 'test4'
     }
-    const graph = new Graph(unsortedJson)
-
-    expect(graph.sortByDistance()).toStrictEqual(sortedJson)
-  })
-
-  test('Test function sortByPromotions(), graph sort by number of promotions', () => {
-    const unsortedJson = example
-    const sortedJson = {
-      1: 'Godfather',
-      2: 'Black mass',
-      3: 'Training day'
-    }
-    const graph = new Graph(unsortedJson)
-
-    expect(graph.sortByPromotions()).toStrictEqual(sortedJson)
-  })
-
-  test('Test function sort()', () => {
-    const unsortedJson = example
-    const sortedJson = {
-      1: 'Godfather',
-      2: 'Training day',
-      3: 'Black mass'
-    }
-    const graph = new Graph(unsortedJson)
-
-    expect(graph.sort()).toStrictEqual(sortedJson)
+    expect(graph.toMovieRecommendationList(exampleRecommendationList)).toStrictEqual(recommendationList)
   })
 })
