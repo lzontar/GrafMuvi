164c3dcfd6612b8b049b420c4d1eda252029f98f
lzontar <44132336+lzontar@users.noreply.github.com>
2019-10-17 14:43:02 +0200

    Closes #67

--- __test__/api.test.js
+++ __test__/api.test.js
@@ -1,28 +1,39 @@
 // Author: @lzontar
 
 import request from 'supertest'
-const app = require('../src/api/server')
+const app = require('../src/api/server').app
+const server = require('../src/api/server').server
+const database = require('../src/api/database')
+const neo4jConnection = require('../src/api/server').database
+
+beforeAll(() => {
+  database.initTestData(neo4jConnection.session)
+})
 
 describe('Get Endpoints', () => {
   it('Get request movie recommendation by id', (done) => {
     request(app)
-      .get('/api/id/tt0765429')
+      .get('/api/id/123')
       .expect(200, {
-        1: 'Godfather',
-        2: 'Training day',
-        3: 'Black mass'
+        1: 'test2',
+        2: 'test3',
+        3: 'test4'
       })
-      .end(() => done());
+      .end(() => {
+        done()
+      })
   })
   it('Get request movie recommendation by title', (done) => {
     request(app)
-      .get('/api/title/American%20Gangster/2007')
+      .get('/api/title/test1/2019')
       .expect(200, {
-        1: 'Godfather',
-        2: 'Training day',
-        3: 'Black mass'
+        1: 'test2',
+        2: 'test3',
+        3: 'test4'
       })
-      .end(() => done());
+      .end(() => {
+        done()
+      })
   })
 })
 
@@ -31,32 +42,44 @@ describe('Post Endpoints', () => {
     request(app)
       .post('/api/id')
       .send({
-        imdbId1: 'tt0765429',
-        imdbId2: 'tt0353496',
+        imdbId1: '123',
+        imdbId2: '456',
         downgrade: false
       })
       .expect(200, {
-        id1: 'tt0765429',
-        id2: 'tt0353496',
+        id1: '123',
+        id2: '456',
         promotions: 101
       })
-      .end(() => done());
+      .end(() => {
+        done()
+      })
   })
   it('Post request movie recommendation promotion by title', (done) => {
     request(app)
       .post('/api/title')
       .send({
-        title1: 'American gangster',
-        released1: 2007,
-        title2: 'Godfather',
-        released2: 1991,
+        title1: 'test1',
+        released1: 2019,
+        title2: 'test2',
+        released2: 2019,
         downgrade: true
       })
       .expect(200, {
-        id1: 'tt0765429',
-        id2: 'tt0353496',
+        id1: '123',
+        id2: '456',
         promotions: 99
       })
-      .end(() => done());
+      .end(() => {
+        done()
+      })
   })
+})
+
+afterAll(() => {
+  database.clearTestData(neo4jConnection.session, neo4jConnection.driver, function (driver, session) {
+    driver.close()
+    session.close()
+  })
+  server.close()
 })
