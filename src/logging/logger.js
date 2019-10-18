dc8ba7370358ee14817eb2ab2c8bcea1d3d89c48
lzontar <44132336+lzontar@users.noreply.github.com>
2019-10-19 01:24:54 +0200

    Closes #81

--- src/logging/logger.js
+++ src/logging/logger.js
@@ -1,5 +1,10 @@
 const winston = require('winston')
+const SentryTransport = require('@synapsestudios/winston-sentry')
+const Sentry = require('@sentry/node')
+
 var appRoot = require('app-root-path')
+
+Sentry.init({ dsn: 'https://2ec26f6a5f61480599c3568c6e362569@sentry.io/1784355' });
 
 const logConfiguration = {
   transports: [
@@ -29,7 +34,8 @@ const logConfiguration = {
         winston.format.colorize(),
         winston.format.simple()
       )
-    })
+    }),
+    new SentryTransport({ Sentry })
   ]
 }
 const logger = winston.createLogger(logConfiguration)
