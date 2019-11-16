b3e5f37bb35dc1fbaf42d0a282156ad4b501a853
lzontar <44132336+lzontar@users.noreply.github.com>
2019-11-16 18:45:48 +0100

    Syntax fix in server.js, advances #96

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const api = new GrafMuvi()
 
 const app = express()
 const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080
-const ip = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+const ip = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 app.use(express.json())
 
 const neoDriver = database.connectDB()
